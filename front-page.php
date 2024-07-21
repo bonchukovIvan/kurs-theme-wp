1f230de90437220b74f1cf0d6626e3b53e483818
BonchukovIvan <bonchukooov@gmail.com>
2024-07-21 15:54:57 +0300

    feat: add style for another sections

--- front-page.php
+++ front-page.php
@@ -71,36 +71,44 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 <section class="advantages" style="background-image: url(<?php echo KURSTHEME_THEME_URI.'/assets/img/bck2.png'; ?>);">
     <!--  -->
-    <div class="container">
+
+    <div class="container" style="position: relative;">
+    <div class="advantages__back">
+        <img src="<?php echo KURSTHEME_THEME_URI.'/assets/img/flower-1.png'?>" alt="">
+    </div>
+    <div class="advantages__flowers">
+        <img src="<?php echo KURSTHEME_THEME_URI.'/assets/img/flower-2.png'?>" alt="">
+        <img src="<?php echo KURSTHEME_THEME_URI.'/assets/img/flower-3.png'?>" alt="">
+    </div>
         <div class="advantages__body">
             <div class="advantages__title">
                 <h3>ПЕРЕВАГИ КУРСУ</h3>
-                <img src="" alt="">
+                <img src="<?php echo KURSTHEME_THEME_URI.'/assets/img/asset-title-2.png'?>" alt="">
             </div>
-            <div class="about-kurs__cards">
-                <div class="about-kurs__card">
-                    <div class="about-kurs__number">01</div>
+            <div class="advantages__cards">
+                <div class="advantages__card">
+                    <div class="advantages__number">01</div>
                     <p>Я говорю з учнем тією мовою, яку він почує. <span class="t-orange">Жодних «офіціозів» та упереджень.</span></p>
                 </div>
-                <div class="about-kurs__card">
-                    <div class="about-kurs__number">01</div>
-                    <p>Я говорю з учнем тією мовою, яку він почує. <span class="t-orange">Жодних «офіціозів» та упереджень.</span></p>
+                <div class="advantages__card">
+                    <div class="advantages__number">02</div>
+                    <p>Впродовж 3 років я виточувала власну систему занять, котра справді ДІЄ! Мої учні не зазубрюють, вони – розуміють.</p>
                 </div>
-                <div class="about-kurs__card">
-                    <div class="about-kurs__number">01</div>
-                    <p>Я говорю з учнем тією мовою, яку він почує. <span class="t-orange">Жодних «офіціозів» та упереджень.</span></p>
+                <div class="advantages__card">
+                    <div class="advantages__number">03</div>
+                    <p>Курс читається на групових заняттях, проте я завжди відкрита до діалогу та без проблем проведу індивідуальні заняття тим, хто трохи відстав або прагне знати більше.</p>
                 </div>
-                <div class="about-kurs__card">
-                    <div class="about-kurs__number">01</div>
-                    <p>Я говорю з учнем тією мовою, яку він почує. <span class="t-orange">Жодних «офіціозів» та упереджень.</span></p>
+                <div class="advantages__card">
+                    <div class="advantages__number">04</div>
+                    <p>Моя програма складена на основі авторського підручника, який доповнить курс безоплатно!</p>
                 </div>
-                <div class="about-kurs__card">
-                    <div class="about-kurs__number">01</div>
-                    <p>Я говорю з учнем тією мовою, яку він почує. <span class="t-orange">Жодних «офіціозів» та упереджень.</span></p>
+                <div class="advantages__card">
+                    <div class="advantages__number">05</div>
+                    <p>Година наших занять мине безболісно: з жартами, реальними прикладами та живим діалогом.</p>
                 </div>
-                <div class="about-kurs__card">
-                    <div class="about-kurs__number">01</div>
-                    <p>Я говорю з учнем тією мовою, яку він почує. <span class="t-orange">Жодних «офіціозів» та упереджень.</span></p>
+                <div class="advantages__card">
+                    <div class="advantages__number">06</div>
+                    <p>Я завжди слідкую за результатами учнів та розумію прагнення батьків бачити прогрес від занять зі мною. Жоден онлайн-урок не проходить без домашньої роботи та її ретельної перевірки!</p>
                 </div>
             </div>
         </div>
@@ -109,47 +117,56 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 <section class="reviews">
     <!--  -->
-    <div class="container">
+    <div class="container" style="position: relative;">
+    <div class="reviews__back">
+        <img src="<?php echo KURSTHEME_THEME_URI.'/assets/img/grid.png'?>" alt="">
+    </div>
         <div class="reviews__title">
-            <h3>ВІДГУКИ</h3>
-            <img src="" alt="">
+            <div>
+                <h3>ВІДГУКИ</h3>
+                <img src="<?php echo KURSTHEME_THEME_URI.'/assets/img/asset-title-1.png'?>" alt="">
+            </div>
         </div>
         <div class="reviews__body">
             <div class="reviews__items">
-                <div class="reviews__item">
-                    <h2>Аня, 186</h2>
-                    <p>Маргарита досить уважна, знає свій матеріал ну просто на зубок. Уроки проходять завжди відмінно, а якщо мені щось незрозуміло, я можу завжди перепитати, дізнатися, а вона пояснить: що? як? чому? В цілому я дуже задоволена курсом!</p>
+                <div class="reviews__item" id="review1">
+                    <div>
+                        <h2>Аня, 186</h2>
+                        <p>Маргарита досить уважна, знає свій матеріал ну просто на зубок. Уроки проходять завжди відмінно, а якщо мені щось незрозуміло, я можу завжди перепитати, дізнатися, а вона пояснить: що? як? чому? В цілому я дуже задоволена курсом!</p>
+                    </div>
+                    <img src="<?php echo KURSTHEME_THEME_URI.'/assets/img/r1.png'?>" alt="">
                 </div>
-                <img src="" alt="">
-            </div>
-            <div class="reviews__items">
-                <div class="reviews__item">
-                    <h2>Аня, 186</h2>
-                    <p>Маргарита досить уважна, знає свій матеріал ну просто на зубок. Уроки проходять завжди відмінно, а якщо мені щось незрозуміло, я можу завжди перепитати, дізнатися, а вона пояснить: що? як? чому? В цілому я дуже задоволена курсом!</p>
+                <div class="reviews__item" id="review2">
+                    <div>
+                        <h2>СОФІЯ, 178</h2>
+                        <p>Маргарита - чудова репетиторка, вчить за своїми матеріалами та зрозуміло, часто з якимось гумором викладає свій предмет. Подобається те, що після кожної пройденої теми дається тест на рівень знання пройденого навчального матеріалу, де твої неправильні відповіді не ігноруються, а повторно розбираються.</p>
+                    </div>
+                    <img src="<?php echo KURSTHEME_THEME_URI.'/assets/img/r2.png'?>" alt="">
                 </div>
-                <img src="" alt="">
-            </div>
-            <div class="reviews__items">
-                <div class="reviews__item">
-                    <h2>Аня, 186</h2>
-                    <p>Маргарита досить уважна, знає свій матеріал ну просто на зубок. Уроки проходять завжди відмінно, а якщо мені щось незрозуміло, я можу завжди перепитати, дізнатися, а вона пояснить: що? як? чому? В цілому я дуже задоволена курсом!</p>
+                <div class="reviews__item" id="review3">
+                    <div>
+                        <h2>МАРІЯ, 190</h2>
+                        <p>Маргарита стала моєю першою репетиторкою, проте враження від уроків приємні! Працювати з нею легко, все зрозуміло та доступно, цікаво. </p>
+                    </div>
+                    <img src="<?php echo KURSTHEME_THEME_URI.'/assets/img/r3.png'?>" alt="">
                 </div>
-                <img src="" alt="">
-            </div>
-            <div class="reviews__items">
-                <div class="reviews__item">
-                    <h2>Аня, 186</h2>
-                    <p>Маргарита досить уважна, знає свій матеріал ну просто на зубок. Уроки проходять завжди відмінно, а якщо мені щось незрозуміло, я можу завжди перепитати, дізнатися, а вона пояснить: що? як? чому? В цілому я дуже задоволена курсом!</p>
+                <div class="reviews__item" id="review4">
+                    <div>
+                        <h2>Богдан, 195</h2>
+                        <p>Міцненько, добренько. Уроки ємні, без зайвої інформації, але з цікавими фактами. Особливо запам'яталася розповідь про Олену Вітер. Гарний бал я отримав через правильно розставлені акценти зі вчительської сторони.</p>
+                    </div>
+                    <img src="<?php echo KURSTHEME_THEME_URI.'/assets/img/r4.png'?>" alt="">
+                </div>  
+                <div class="reviews__item" id="review5">
+                    <div>
+                        <h2>НАСТЯ, 184</h2>
+                        <p>Маргарита чудово знаходить підхід до кожного учня та дає найпотрібнішу інформацію в цікавому вигляді. Її спосіб навчання допоміг мені краще засвоїти матеріал. Пам'ятаю, як сиділа на іспиті та побачила портрет Сагайдачного, і в мене в голові пролунав голос Маргарити, який розповідав, чим же таким важливим нам ця особа запам'яталася.</p>
+                    </div>
+                    <img src="<?php echo KURSTHEME_THEME_URI.'/assets/img/r5.png'?>" alt="">
                 </div>
-                <img src="" alt="">
             </div>
-            <div class="reviews__items">
-                <div class="reviews__item">
-                    <h2>Аня, 186</h2>
-                    <p>Маргарита досить уважна, знає свій матеріал ну просто на зубок. Уроки проходять завжди відмінно, а якщо мені щось незрозуміло, я можу завжди перепитати, дізнатися, а вона пояснить: що? як? чому? В цілому я дуже задоволена курсом!</p>
-                </div>
-                <img src="" alt="">
-            </div>
+   
+
         </div>
     </div>
 </section>
--- template-parts/footer/footer-main.php
+++ template-parts/footer/footer-main.php
@@ -4,32 +4,34 @@
             <div class="m-footer__logo">
                 <img src="<?php echo KURSTHEME_THEME_URI.'/assets/img/logo.svg'?>" alt="">
             </div>
-            <?php wp_nav_menu([
-                    'menu'                 => '',
-                    'container'            => 'div',
-                    'container_class'      => 'm-header__navigation',
-                    'container_id'         => '',
-                    'container_aria_label' => '',
-                    'menu_class'           => 'm-header__menu',
-                    'menu_id'              => '',
-                    'echo'                 => true,
-                    'fallback_cb'          => 'wp_page_menu',
-                    'before'               => '',
-                    'after'                => '',
-                    'link_before'          => '',
-                    'link_after'           => '',
-                    'items_wrap'           => '<ul id="%1$s" class="%2$s wbsmd-menu">%3$s</ul>',
-                    'item_spacing'         => 'preserve',
-                    'depth'                => 0,
-                    'walker'               => '',
-                    'theme_location'       => 'main_header_menu',
-            ]); ?>
-            <div class="m-footer__icons">
-                <div>
-                    <img src="<?php echo KURSTHEME_THEME_URI.'/assets/img/phone.svg'?>" alt="">
-                </div>
-                <div>
-                    <img src="<?php echo KURSTHEME_THEME_URI.'/assets/img/inst.svg'?>" alt="">
+            <div class="m-footer__mobile">
+                <?php wp_nav_menu([
+                        'menu'                 => '',
+                        'container'            => 'div',
+                        'container_class'      => 'm-footer__navigation',
+                        'container_id'         => '',
+                        'container_aria_label' => '',
+                        'menu_class'           => 'm-footer__menu',
+                        'menu_id'              => '',
+                        'echo'                 => true,
+                        'fallback_cb'          => 'wp_page_menu',
+                        'before'               => '',
+                        'after'                => '',
+                        'link_before'          => '',
+                        'link_after'           => '',
+                        'items_wrap'           => '<ul id="%1$s" class="%2$s wbsmd-menu">%3$s</ul>',
+                        'item_spacing'         => 'preserve',
+                        'depth'                => 0,
+                        'walker'               => '',
+                        'theme_location'       => 'main_header_menu',
+                ]); ?>
+                <div class="m-footer__icons">
+                    <div>
+                        <img src="<?php echo KURSTHEME_THEME_URI.'/assets/img/phone.svg'?>" alt="">
+                    </div>
+                    <div>
+                        <img src="<?php echo KURSTHEME_THEME_URI.'/assets/img/inst.svg'?>" alt="">
+                    </div>
                 </div>
             </div>
         </div>
